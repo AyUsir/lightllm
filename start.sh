a5dd83e5d9d642e6577f83b87cfd46ebb64a61a0
景凯文 <95631053+AyUsir@users.noreply.github.com>
2024-03-29 19:38:39 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+git lfs install
+git clone https://huggingface.co/Qwen/Qwen1.5-7B-Chat
+python -m lightllm.server.api_server --model_dir Qwen1.5-7B-chat/     \
+                                     --host 0.0.0.0                 \
+                                     --port 8080                    \
+                                     --tp 1                         \
+                                     --eos_id 151645 \
+                                     --trust_remote_code \
+                                     --max_total_token_num 120000
