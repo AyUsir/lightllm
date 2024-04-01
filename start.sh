6e0c9d0c65535a8b81033d2d74ce8294fdeb4c99
景凯文 <95631053+AyUsir@users.noreply.github.com>
2024-04-01 14:52:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 git lfs install
 git clone https://huggingface.co/Qwen/Qwen1.5-7B-Chat
-python -m lightllm.server.api_server --model_dir Qwen1.5-7B-chat/     \
+python -m lightllm.server.api_server --model_dir Qwen1.5-7B-Chat/     \
                                      --host 0.0.0.0                 \
                                      --port 8080                    \
                                      --tp 1                         \
